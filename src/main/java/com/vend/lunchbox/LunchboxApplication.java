4f31704aa3d0d21a8b34ac3d2c2a2db8922186e8
shoaib4330 <shoaib.anwar@venturedive.com>
2018-10-23 11:25:43 +0500

    created and pushed project

---
+++ src/main/java/com/vend/lunchbox/LunchboxApplication.java
@@ -0,0 +1,12 @@
+package com.vend.lunchbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchboxApplication.class, args);
+    }
+}
