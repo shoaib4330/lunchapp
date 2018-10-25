b95f894e5f26c31c65934a512cb8bd28c47d4aa7
SheikhHamza <hamzasajjaditya@gmail.com>
2018-10-25 13:58:45 +0500

    made it production ready

--- src/main/java/com/venturedive/rotikhilao/RotikhilaoApplication.java
+++ src/main/java/com/venturedive/rotikhilao/RotikhilaoApplication.java
@@ -3,12 +3,17 @@ package com.venturedive.rotikhilao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@Slf4j
-public class RotikhilaoApplication {
+public class RotikhilaoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RotikhilaoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RotikhilaoApplication.class, args);
